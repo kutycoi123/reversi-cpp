e04e21102493444f3cc9797471f42e2271a9e29c
kutycoi123 <kutycoi123@gmail.com>
2020-08-04 14:12:37 -0700

    Cleanup

--- main.cpp
+++ main.cpp
@@ -9,54 +9,6 @@
 #include <stdlib.h>
 #include "reversi.h"
 
-/*struct Move{
-	int i, j;
-};	
-
-inline bool operator==(const Move& lhs, const Move& rhs) {
-	return lhs.i == rhs.i && lhs.j == rhs.j;
-}
-inline bool operator!=(const Move& lhs, const Move& rhs) {
-	return !(lhs == rhs);
-}
-*/
-/*
-class Player {
-public:
-	int id;
-	int score;
-	std::string name;
-	virtual Move next_move(const std::vector<Move>&) = 0;
-	virtual void acknowledge(const std::vector<Move>&, int) = 0;
-	void set_id(int id) {
-		this->id = id;
-	}
-	Player() : score(0) {}
-	Player(const std::string& name) : score(0), name(name){}
-	~Player(){}
-};
-*/
-class Human : public Player {
-public:
-	Move next_move(const std::vector<Move>& possible_moves) override {
-		int i, j;
-		std::cout << "Choose row: ";
-		std::cin >> i;
-		std::cout << "Choose column: ";
-		std::cin >> j;
-		return Move{i,j};
-	}
-	void acknowledge(const std::vector<Move>& moves, int id) override {
-
-	}
-	Human() : Player() {}
-	Human(const std::string& name) : Player(name) {}
-	~Human(){}
-};
-/*
-int get_opponent_id(int id) {
-	return id * -1;
-}
 void display_board(int board[N][N], const Player* ply1, const Player*ply2, const std::vector<Move>& moves = {}) {
 	std::cout << " ";
 	for (int i = 0; i < N; ++i) {
@@ -84,91 +36,7 @@ void display_board(int board[N][N], const Player* ply1, const Player*ply2, const
 	std::cout << ply1->name << ": " << ply1->score << "\n";
 	std::cout << ply2->name << ": " << ply2->score << "\n";
 }
-bool find_player_move(int board[N][N], int id, Move start, int i_delta, int j_delta) {
-	for (int start_i = start.i, start_j=start.j;
-		start_i >= 0 && start_i < N && start_j >= 0 && start_j < N;
-		start_i += i_delta, start_j += j_delta) {
-		if (board[start_i][start_j] == 0) {
-			return false;
-		}
-		else if (board[start_i][start_j] == id) {
-			return true;
-		}
-	}
-	return false;
-}
-std::vector<Move> possible_moves(int board[N][N], int id) {
-	std::vector<Move> moves;
-	auto opponent_id = get_opponent_id(id);
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (board[i][j] == 0) {
-				bool legal = (i < N - 1 && board[i+1][j] == opponent_id && find_player_move(board, id, Move{i+1, j}, 1, 0)) || 
-							 (i >= 1 && board[i-1][j] == opponent_id && find_player_move(board, id, Move{i-1,j}, -1, 0)) || 
-							 (j < N - 1 && board[i][j+1] == opponent_id && find_player_move(board, id, Move{i,j+1}, 0, 1)) || 
-							 (j >= 1 && board[i][j-1] == opponent_id && find_player_move(board, id, Move{i,j-1}, 0, -1)) || 
-							 (i < N - 1 && j < N - 1 && board[i+1][j+1] == opponent_id && find_player_move(board, id, Move{i+1, j+1}, 1, 1)) ||
-							 (i >= 1 && j >= 1 && board[i-1][j-1] == opponent_id && find_player_move(board, id, Move{i-1, j-1}, -1, -1)) || 
-							 (i >= 1 && j < N - 1 && board[i-1][j+1] == opponent_id && find_player_move(board, id, Move{i-1, j+1}, -1, 1)) || 
-							 (i < N - 1 && j >= 1 && board[i+1][j-1] == opponent_id && find_player_move(board, id, Move{i+1, j-1}, 1, -1));
-				if (legal) {
-					moves.push_back(Move{i, j});
-				}
-			}
-		}
-	}
 
-	return moves;
-}
-int get_winner(int board[N][N]) {
-	int ply1_count = 0, ply2_count = 0;
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
-			if (board[i][j] == -1) 
-				ply1_count++;
-			else if (board[i][j] == 1) 
-				ply2_count++;
-		}
-	}
-	if (ply1_count > ply2_count) 
-		return -1;
-	else if (ply1_count < ply2_count)
-		return 1;
-	else
-		return 0;
-}
-std::vector<Move> update(int board[N][N], Move move, int id) {
-	std::vector<Move> new_moves;
-	new_moves.push_back(move);
-	board[move.i][move.j] = id;
-	auto opponent_id = get_opponent_id(id);
-	std::vector<std::pair<int,int>> deltas;
-	deltas.push_back(std::make_pair(0,1));
-	deltas.push_back(std::make_pair(0,-1));
-	deltas.push_back(std::make_pair(1,0));
-	deltas.push_back(std::make_pair(-1,0));
-	deltas.push_back(std::make_pair(1,-1));
-	deltas.push_back(std::make_pair(-1,1));
-	deltas.push_back(std::make_pair(-1,-1));
-	deltas.push_back(std::make_pair(1,1));
-	for (const auto& delta : deltas) {
-		if (move.i < N && move.i >= 0 && move.j < N && move.j >= 0 && 
-			find_player_move(board, id, Move{move.i+delta.first, move.j+delta.second}, delta.first, delta.second)) {
-			for (int i = move.i, j = move.j;
-				i + delta.first < N && i + delta.first >= 0 && j + delta.second < N && j + delta.second >= 0;
-				i += delta.first, j += delta.second) {
-				if (board[i+delta.first][j+delta.second] == opponent_id) {
-					board[i+delta.first][j+delta.second] = id;
-					new_moves.push_back(Move{i+delta.first, j+delta.second});
-				}else {
-					break;
-				}
-			}
-		}
-	}
-	return new_moves;
-}
-*/
 class Reversi {
 public:
 	int curr_player_id;
@@ -190,10 +58,11 @@ public:
 		// board[N/2][N/2-1] = 1;
 		// board[N/2][N/2] = -1;
 
-		board[N/2-1][N/2-1] = 1;
-		board[N/2-1][N/2] = -1;
-		board[N/2][N/2-1] = -1;
-		board[N/2][N/2] = 1;
+		// board[N/2-1][N/2-1] = 1;
+		// board[N/2-1][N/2] = -1;
+		// board[N/2][N/2-1] = -1;
+		// board[N/2][N/2] = 1;
+		init_board(board);
 		this->ply1->score = 2;
 		this->ply2->score = 2;
 		curr_player_id = 1; // first player
@@ -256,162 +125,7 @@ private:
 
 
 
-struct TreeNode {
-	int board[N][N];
-	int nVisited = 0;
-	int nWins = 0;
-	int player_id; //player about to take move
-	Move move;
-	TreeNode* parent = NULL;	
-	std::vector<std::unique_ptr<TreeNode>> children;
-	TreeNode() {}
-	TreeNode(int player_id, int b[N][N], TreeNode* par) : player_id(player_id), parent(par) {
-		for (int i = 0; i < N; ++i) {
-			for (int j = 0; j < N; ++j) {
-				board[i][j] = b[i][j];
-			}
-		}
-	}
-	TreeNode* best_child() {
-		auto heu = [](const TreeNode& node){
-			if (node.nVisited == 0 || node.parent->nVisited == 0) {
-				return INF;
-			}
-			return (double)node.nWins / node.nVisited + 1.4*(sqrt(log((double)node.parent->nVisited)) / node.nVisited);
-		};
-		double max = -1;
-		TreeNode* best_child = NULL;
-		for (auto& child : children) {
-			double heu_score = heu(*child);
-			if (heu_score > max) {
-				max = heu_score;
-				best_child = child.get();
-			}
-		}
-		return best_child;
-	}
 
-};
-
-
-class AI : public Player {
-public:
-	int board[N][N];
-	void acknowledge(const std::vector<Move>& moves, int id) override {
-		for (auto& move : moves) {
-			board[move.i][move.j] = id;
-		}
-	}
-	Move next_move(const std::vector<Move>& possible_moves) override {
-		std::cout << "moves size: " << possible_moves.size() << "\n";
-		auto best_move = monte_carlo_tree_search(board);
-		// board[best_move.i][best_move.j] = this->id;
-		update(board, best_move, this->id);
-		return best_move;
-	}
-	AI() : Player() {
-		// board[N/2-1][N/2-1] = -1;
-		// board[N/2-1][N/2] = 1;
-		// board[N/2][N/2-1] = 1;
-		// board[N/2][N/2] = -1;
-		board[N/2-1][N/2-1] = 1;
-		board[N/2-1][N/2] = -1;
-		board[N/2][N/2-1] = -1;
-		board[N/2][N/2] = 1;		
-	}
-	AI(const std::string& name) : Player(name) {
-		// board[N/2-1][N/2-1] = -1;
-		// board[N/2-1][N/2] = 1;
-		// board[N/2][N/2-1] = 1;
-		// board[N/2][N/2] = -1;
-		board[N/2-1][N/2-1] = 1;
-		board[N/2-1][N/2] = -1;
-		board[N/2][N/2-1] = -1;
-		board[N/2][N/2] = 1;		
-	}
-
-	TreeNode* select(TreeNode* node) {
-		if (node != NULL && node->children.size() != 0) {
-			return select(node->best_child());
-		}
-		return node;
-	}
-	TreeNode* expand(TreeNode* node) {
-		if (node->children.size() == 0 ) {
-			auto moves = possible_moves(node->board, node->player_id);
-			if (moves.size() > 0) {
-				for (auto& move : moves) {
-					auto child = std::make_unique<TreeNode>(node->player_id*-1, node->board, node);
-					child->move = Move{move.i, move.j};
-					// child->board[move.i][move.j] = node->player_id;
-					update(child->board, child->move, node->player_id);
-					node->children.push_back(std::move(child));
-				}
-			}
-		}
-		return select(node);
-	}
-	int playout(TreeNode* node) {
-		auto cur_player = node->player_id;
-		int copied_board[N][N];
-		for (int i = 0; i < N; ++i) {
-			for (int j = 0; j < N; ++j) {
-				copied_board[i][j] = node->board[i][j];
-			}
-		}
-		while (true) {
-			// Get all possible moves
-			auto moves = possible_moves(copied_board, cur_player);
-			if (moves.size() == 0) {
-				break;
-			}
-			// Randomly choose a move among possible moves
-			int rand_index = rand() % moves.size();
-			Move rand_move = moves[rand_index];
-			// Update the copied_board
-			update(copied_board, rand_move, cur_player);
-			cur_player *= -1;
-		}
-		int winner = get_winner(copied_board);
-		if (winner == 0) 
-			return 0.5;
-		if (winner == node->player_id)
-			return -1;
-		return 1;
-	}
-	void backprop(TreeNode* node, int result){
-		if (result > 0) {
-			node->nWins += result;
-		}
-		node->nVisited++;
-		if (node->parent) {
-			backprop(node->parent, -result);
-		}
-	}
-	Move monte_carlo_tree_search(int board[N][N], int NTIMES = 5000) {
-		TreeNode root(id, board, NULL);
-		for (int i = 0; i < NTIMES; ++i) {
-			auto leaf = select(&root);
-			auto child = expand(leaf);
-			//return Move{0,0};
-			auto result = playout(child);
-			backprop(child, result);
-		}
-		//display_board(board, NULL, NULL, possible_moves(root.board, root.player_id));
-		// auto moves = possible_moves(root.board, root.player_id);
-		// std::cout << "Here\n" << root.children.size() << "\n";
-		// std::cout << "possible moves size: " << moves.size() << "\n";
-		auto best_child = root.children[0].get();
-		for (auto& child : root.children) {
-			std::cout << child->move.i << " " << child->move.j << "\n";
-			if (child->nVisited > best_child->nVisited) {
-				best_child = child.get();
-			}
-		}
-		return best_child->move;
-	}
-	~AI(){}
-};
 
 int main(int argc, char**argv) {
 	srand (time(NULL));
